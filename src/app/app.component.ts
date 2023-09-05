310eeefd7f4fe67e671cb45677b6511caa654ecf
Makis-official <nesterenkomakar@yandex.ru>
2023-09-05 23:19:59 +0300

    TypeScript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ResponseService } from './service/response.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FirstAngularApp';
+
+  constructor(public RespServ: ResponseService) {}
 }
---
+++ src/app/service/response.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { OneEl } from '../interface/one-el';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ResponseService {
+  
+
+  constructor() { }
+
+  one: OneEl[] = [
+    {
+      userId: 1,
+      id: 1,
+      title: "delectus aut autem",
+      completed: false
+    },
+    {
+      userId: 1,
+      id: 2,
+      title: "quis ut nam facilis et officia qui",
+      completed: false
+    },
+    {
+      userId: 1,
+      id: 3,
+      title: "fugiat veniam minus",
+      completed: false
+    },
+  ]
+}
