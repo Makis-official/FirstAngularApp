7e573531780d598f23f7ff92c145042593fde378
Makis-official <nesterenkomakar@yandex.ru>
2023-09-06 21:13:42 +0300

    Forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
 
+interface Books {
+  name_book: string | null;
+  author: string | null;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'FirstAngularApp';
+
+  books: Books[] = [
+    {
+      name_book: 'Идиот',
+      author: 'Достоевский'
+    },
+    {
+      name_book: "Щоколад",
+      author: "Харрис"
+    },
+    {
+      name_book: "Ревиззор",
+      author: "Гоголь"
+    }
+  ];
+
+  create_Books = {
+    name_book: null,
+    author: null,
+  }
+
+  addBook() {
+    this.books.push({
+      name_book: this.create_Books.name_book,
+      author: this.create_Books.author
+    });
+  }
+
 }
