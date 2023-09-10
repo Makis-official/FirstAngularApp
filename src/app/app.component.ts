fe4b862f0c5b9698e6570a1a04a4161f220471f9
Makis-official <nesterenkomakar@yandex.ru>
2023-09-10 17:44:01 +0300

    RxJs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import {Subscription, interval, map} from 'rxjs';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FirstAngularApp';
+
+  intervalSub$!: Subscription;
+  intervalRundSub$!: Subscription;
+
+  counterMas: Number[] = [];
+  randMas: String[] = [];
+
+  enableStopButton = false;
+  enableRandStopButton = false;
+
+  constructor() {}
+
+  enableInterval(){
+    const intervalStreaming = interval(2000);
+
+    this.intervalSub$ = intervalStreaming.subscribe((value) => {
+      this.counterMas.push(Number(value));
+      console.log(value);
+    });
+
+
+    this.intervalRundSub$ = intervalStreaming.pipe(map(() => `Random Value: ${Math.floor(Math.random() * (100)) + 1}`)).subscribe((value)=> {
+      this.randMas.push(value);
+      console.log(value);
+    }
+      );
+
+    this.enableStopButton = true;
+    this.enableRandStopButton = true;
+  };
+
+
+    stopSubs() {
+    this.intervalSub$.unsubscribe();
+    this.enableStopButton = false;
+  };
+
+  stopRandSubs() {
+    this.intervalRundSub$.unsubscribe();
+    this.enableRandStopButton = false;
+  };
 }
