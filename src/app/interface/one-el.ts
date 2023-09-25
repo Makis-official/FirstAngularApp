1bb8cbc68859bf707c455d34714b20d32978a614
Makis-official <nesterenkomakar@yandex.ru>
2023-09-25 22:06:17 +0300

    code style

--- src/app/interface/one-el.ts
+++ src/app/interface/one-el.ts
@@ -1,6 +1,6 @@
 export interface OneEl {
-    userId: number,
-    id: number,
-    title: string,
-    completed: Boolean
+    userId: number;
+    id: number;
+    title: string;
+    completed: Boolean;
 }
--- src/app/service/response.service.ts
+++ src/app/service/response.service.ts
@@ -11,20 +11,20 @@ export class ResponseService {
       userId: 1,
       id: 1,
       title: "delectus aut autem",
-      completed: false
+      completed: false,
     },
     {
       userId: 1,
       id: 2,
       title: "quis ut nam facilis et officia qui",
-      completed: false
+      completed: false,
     },
     {
       userId: 1,
       id: 3,
       title: "fugiat veniam minus",
-      completed: false
-    }
+      completed: false,
+    },
   ];
   
 }
